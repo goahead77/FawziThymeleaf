0b7406970492bcc50637a256e25a928e593b600f
goahead77 <736694675@qq.com>
2016-10-16 15:44:12 +0800

    环境问题？？？

--- src/main/java/cn/fawzi/thymeleaf/config/MvcConfig.java
+++ src/main/java/cn/fawzi/thymeleaf/config/MvcConfig.java
@@ -3,22 +3,19 @@ package cn.fawzi.thymeleaf.config;
 import cn.fawzi.thymeleaf.formatter.DateFormatter;
 import cn.fawzi.thymeleaf.formatter.FoodsFormatter;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring4.view.ThymeleafView;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
-
-import java.util.Collections;
 
 /**
  * @author wenqi
@@ -41,32 +38,20 @@ public class MvcConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 
     @Bean
     public SpringResourceTemplateResolver templateResolver(){
-        // SpringResourceTemplateResolver automatically integrates with Spring's own
-        // resource resolution infrastructure, which is highly recommended.
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
         templateResolver.setApplicationContext(this.applicationContext);
         templateResolver.setPrefix("/WEB-INF/templates/");
         templateResolver.setSuffix(".html");
         templateResolver.setCharacterEncoding("UTF-8");
-        // HTML is the default value, added here for the sake of clarity.
         templateResolver.setTemplateMode(TemplateMode.HTML);
-        // Template cache is true by default. Set to false if you want
-        // templates to be automatically updated when modified.
         templateResolver.setCacheable(true);
         return templateResolver;
     }
 
     @Bean
     public SpringTemplateEngine templateEngine(){
-        // SpringTemplateEngine automatically applies SpringStandardDialect and
-        // enables Spring's own MessageSource message resolution mechanisms.
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(templateResolver());
-        // Enabling the SpringEL compiler with Spring 4.2.4 or newer can
-        // speed up execution in most scenarios, but might be incompatible
-        // with specific cases when expressions in one template are reused
-        // across different data types, so this flag is "false" by default
-        // for safer backwards compatibility.
         templateEngine.setEnableSpringELCompiler(true);
         return templateEngine;
     }
@@ -75,9 +60,6 @@ public class MvcConfig extends WebMvcConfigurerAdapter implements ApplicationCon
     public ThymeleafViewResolver viewResolver(){
         ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
         viewResolver.setTemplateEngine(templateEngine());
-        // NOTE 'order' and 'viewNames' are optional
-//        viewResolver.setOrder(1);
-//        viewResolver.setViewNames(new String[] {".html", ".xhtml"});
         return viewResolver;
     }
 
@@ -113,14 +95,5 @@ public class MvcConfig extends WebMvcConfigurerAdapter implements ApplicationCon
     @Bean
     public DateFormatter dateFormatter() {
         return new DateFormatter();
-    }
-
-    @Bean
-    @Scope("prototype")
-    public ThymeleafView mainView() {
-        ThymeleafView view = new ThymeleafView("main"); // templateName = 'main'
-        view.setStaticVariables(
-                Collections.singletonMap("footer", "The ACME Fruit Company"));
-        return view;
     }
 }
