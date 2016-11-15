00adaf3fb054eba8ce4854a213306acc11a0e1c8
goahead77 <736694675@qq.com>
2016-11-15 23:24:17 +0800

    error

--- src/main/java/cn/fawzi/thymeleaf/Utils/MD5Util.java
+++ src/main/java/cn/fawzi/thymeleaf/Utils/MD5Util.java
@@ -12,7 +12,7 @@ public class MD5Util {
     public static String string2MD5(String inStr){
         MessageDigest md5 = null;
         try{
-            md5 = MessageDigest.getInstance("Fawzi77");
+            md5 = MessageDigest.getInstance("MD5");
         }catch (Exception e){
             System.out.println(e.toString());
             e.printStackTrace();
