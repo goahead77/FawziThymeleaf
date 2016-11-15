9c89cc8b365c490c1c2b7132678d0dea6079b773
goahead77 <736694675@qq.com>
2016-11-15 23:36:50 +0800

    优化

--- src/main/java/cn/fawzi/thymeleaf/interceptor/AndroidInterceptor.java
+++ src/main/java/cn/fawzi/thymeleaf/interceptor/AndroidInterceptor.java
@@ -1,8 +1,10 @@
 package cn.fawzi.thymeleaf.interceptor;
 
 import cn.fawzi.thymeleaf.Utils.MD5Util;
-import cn.fawzi.thymeleaf.exception.IllegalRequestHeaderException;
+import cn.fawzi.thymeleaf.controller.ApiResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,18 +18,29 @@ public class AndroidInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
+        ObjectMapper objectMapper=new ObjectMapper();
         if(request.getServletPath().startsWith("/android")){
-           String authority= request.getHeader("user-authority");
-            if(!MD5Util.md52String(authority).split(",")[1].equals("fawzi77")){
-                throw new IllegalRequestHeaderException("非法请求");
-            }else{
-                String apiKey=request.getHeader("apiKey");
-                String contentSafety=request.getHeader("Content-Safety");
-                if(StringUtils.isEmpty(authority) || StringUtils.isEmpty(apiKey)|| StringUtils.isEmpty(contentSafety)){
-                    throw new IllegalRequestHeaderException("参数不全");
-                }
-            }
+            try{
+                String authority= request.getHeader("user-authority");
+                String md5Msg=MD5Util.md52String(authority);
+                if(!md5Msg.split(",")[1].equals("fawzi77")){
+                    String result= objectMapper.writeValueAsString(new ApiResult(500,"非法请求",null));
+                    response.getWriter().write(result);
+                    return false;
 
+                }else{
+                    String apiKey=request.getHeader("apiKey");
+                    String contentSafety=request.getHeader("Content-Safety");
+                    if(StringUtils.isEmpty(authority) || StringUtils.isEmpty(apiKey)|| StringUtils.isEmpty(contentSafety)){
+                        String result= objectMapper.writeValueAsString(new ApiResult(500,"参数不能为空",null));
+                        response.getWriter().write(result);
+                        return false;
+                    }
+                }
+            }catch (Exception e){
+                String result= objectMapper.writeValueAsString(new ApiResult(500,"内部错误",null));
+                response.getWriter().write(result);
+            }
         }
         return true;
     }
